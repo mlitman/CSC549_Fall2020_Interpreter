9b1dbe7e64d989fe41b99b6feefae002b3df5782
mlitman <awesomefat@gmail.com>
2020-10-28 10:35:30 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -4,8 +4,31 @@ public class Driver
 
 	public static void main(String[] args) 
 	{
-		System.out.println("hello world!!");
-
+		String s = "A elephant lives in the zoo!";
+		System.out.println(s);
+		String answer = Driver.parse(s);
+		System.out.println(Driver.reverse("woot"));
+	}
+	
+	public static String reverse(String s)
+	{
+		String answer = "";
+		for(int i = 0; i < s.length(); i++)
+		{
+			answer = s.charAt(i) + answer;
+		}
+		return answer;
+	}
+	//write a function that takes a string and replaces all
+	//words (tokens) that are at least 5 character long with
+	//the reverse of that token
+	//"A elephant lives in the zoo!" becomes
+	// “A tnahpele sevil in the zoo!”
+	public static String parse(String s)
+	{
+		String[] parts = s.split(" ");
+		
+		return "woot";
 	}
 
 }
